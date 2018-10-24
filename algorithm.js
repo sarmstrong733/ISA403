f19ae880716745fbc6f51e85fbb743e21d1374f6
akburke <40361371+akburke@users.noreply.github.com>
2018-10-24 14:30:40 -0400

    algorithm updates

--- algorithm.js
+++ algorithm.js
@@ -1,25 +1,25 @@
 document.getElementById("form1").onsubmit=function() {
 	   
-age = parseInt(document.querySelector('input[name = "age"]:checked').value);
+var age = parseInt(document.querySelector('input[name = "age"]:checked').value);
 	   
 	  
-activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
+var activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
 	   
-genre = parseInt(document.querySelector('input[name = "genre"]:checked').value);
+var genre = parseInt(document.querySelector('input[name = "genre"]:checked').value);
 	   
-total= age + activity + genre;
+var total= age + activity + genre;
 	   
 	  
 document.getElementById("answer").innerHTML = total;
 
-	   if(total < 4) {
-document.getElementById("answer2").innerHTML = "You got Taylor Swift
+if(total < 4) {
+document.getElementById("answer2").innerHTML = "You got Taylor Swift.";}
 
-   if(total >=4 && total < 7) {
+else if(total >=4 && total < 7) {
 document.getElementById("answer2").innerHTML = "You got Justin Bieber.";
 }
 	
-	if(total >=7) {
+else{
 document.getElementById("answer2").innerHTML = "You got Maroon 5.";
 }   
 return false; // required to not refresh the page; just leave this here
