a651956bae6e138e8aa71a5f6079072595bd1f2d
akburke <40361371+akburke@users.noreply.github.com>
2018-10-26 12:40:57 -0400

    changes

--- algorithm.js
+++ algorithm.js
@@ -1,21 +1,35 @@
-document.getElementById("submitForm").onsubmit = function () {
-	   
-var age = parseInt(document.querySelector('input[name = "age"]:checked').value);
-	  
-var activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
-	   
-var genre = parseInt(document.querySelector('input[name = "genre"]:checked').value);
-	   
-var total= age + activity + genre;
-	  
-document.getElementById("answer").innerHTML = total;
+var age = 0;
+var activity = 0;
+var meal = 0;
+var vacation = 0;
+var total = 0;
+            
+function displayResult () { 
+    var age = parseInt(document.querySelector('input[name = "season"]:checked').value);
 
-    if (total < 4) {
-        document.getElementById("answer2").innerHTML = "You got Taylor Swift."; 
-    } else if (total >= 4 && total < 7) {
-        document.getElementById("answer2").innerHTML = "You got Justin Bieber."; 
-    } else {
-        document.getElementById("answer2").innerHTML = "You got Maroon 5."; }   
+    var activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
+
+    var meal = parseInt(document.querySelector('input[name = "meal"]:checked').value);
+
+    var vacation = parseInt(document.querySelector('input[name = "vacation"]:checked').value);
+
+    var total= season + activity + meal + vacation;
+
+    document.getElementById("answer").innerHTML = total;
+
+    if(total < 4) {
+        document.getElementById("answer2").innerHTML = "You got Taylor Swift.";}
+    else if(total >=4 && total < 7) {
+        document.getElementById("answer2").innerHTML = "You got Justin Bieber.";
+    }
+    else if{
+        document.getElementById("answer2").innerHTML = "You got Maroon 5.";
+    }  
+    else {
+        alert("Fill out all questions before submitting.");
+    }
     return false; // required to not refresh the page; just leave this here
-};// end the submit function
-	
+}// end the submit function
+window.onload function (){
+    document.getElementById("form1").onclick = displayResult;
+}
