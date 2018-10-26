5b9ead8dcb627d9e3bc3e149913762ce54db3bb7
akburke <40361371+akburke@users.noreply.github.com>
2018-10-25 21:46:45 -0400

    algorithm updates

--- algorithm.js
+++ algorithm.js
@@ -1,27 +1,21 @@
-document.getElementById("form1").onsubmit=function() {
+document.getElementById("submitForm").onsubmit = function () {
 	   
 var age = parseInt(document.querySelector('input[name = "age"]:checked').value);
-	   
 	  
 var activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
 	   
 var genre = parseInt(document.querySelector('input[name = "genre"]:checked').value);
 	   
 var total= age + activity + genre;
-	   
 	  
 document.getElementById("answer").innerHTML = total;
 
-if(total < 4) {
-document.getElementById("answer2").innerHTML = "You got Taylor Swift.";}
-
-else if(total >=4 && total < 7) {
-document.getElementById("answer2").innerHTML = "You got Justin Bieber.";
-}
-	
-else{
-document.getElementById("answer2").innerHTML = "You got Maroon 5.";
-}   
-return false; // required to not refresh the page; just leave this here
-}// end the submit function
+    if (total < 4) {
+        document.getElementById("answer2").innerHTML = "You got Taylor Swift."; 
+    } else if (total >= 4 && total < 7) {
+        document.getElementById("answer2").innerHTML = "You got Justin Bieber."; 
+    } else {
+        document.getElementById("answer2").innerHTML = "You got Maroon 5."; }   
+    return false; // required to not refresh the page; just leave this here
+};// end the submit function
 	
