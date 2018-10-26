7ee2903ef97c65961c185d130293384748095d30
akburke <40361371+akburke@users.noreply.github.com>
2018-10-26 18:23:44 -0400

    itworks

--- algorithm.js
+++ algorithm.js
@@ -1,35 +1,26 @@
-var age = 0;
+var season = 0;
 var activity = 0;
 var meal = 0;
 var vacation = 0;
 var total = 0;
-            
-function displayResult () { 
-    var age = parseInt(document.querySelector('input[name = "season"]:checked').value);
 
-    var activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
+function displayResult() { 
+    season = parseInt(document.querySelector('input[name = "season"]:checked').value);
 
-    var meal = parseInt(document.querySelector('input[name = "meal"]:checked').value);
+    activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
 
-    var vacation = parseInt(document.querySelector('input[name = "vacation"]:checked').value);
+    meal = parseInt(document.querySelector('input[name = "meal"]:checked').value);
 
-    var total= season + activity + meal + vacation;
+    vacation = parseInt(document.querySelector('input[name = "vacation"]:checked').value);
+
+    total = season + activity + meal + vacation;
 
     document.getElementById("answer").innerHTML = total;
 
-    if(total < 4) {
-        document.getElementById("answer2").innerHTML = "You got Taylor Swift.";}
-    else if(total >=4 && total < 7) {
-        document.getElementById("answer2").innerHTML = "You got Justin Bieber.";
-    }
-    else if{
-        document.getElementById("answer2").innerHTML = "You got Maroon 5.";
-    }  
-    else {
-        alert("Fill out all questions before submitting.");
-    }
-    return false; // required to not refresh the page; just leave this here
-}// end the submit function
-window.onload function (){
-    document.getElementById("form1").onclick = displayResult;
+        if (total < 4) {document.getElementById("answer2").innerHTML = "You got Taylor Swift."; } 
+        else if (total >= 4 && total < 7) {document.getElementById("answer2").innerHTML = "You got Justin Bieber."; }
+        else if (total >= 7) {document.getElementById("answer2").innerHTML = "You got Maroon 5."; } 
+        else {alert("Fill out all questions before submitting."); }
+    return false; 
 }
+window.onload = function () {document.getElementById("submitForm").onsubmit = displayResult; };
